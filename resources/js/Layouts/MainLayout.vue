b66b48366ae3a1bc22484ea567b62e3be2a7415f
LidiaGodo <lilula739@gmail.com>
2023-11-26 12:50:45 +0300

    first commit

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="w-3/4 mx-auto">
+        <NavLink :href="route('chats.index')">Chats</NavLink>
+        <slot />
+    </div>
+</template>
+
+<script>
+import NavLink from "@/Components/NavLink.vue";
+export default {
+    name: "MainLayout",
+    components: {NavLink}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <div class="mt-2">
+        Chats
+        <div class="flex mt-3">
+
+            <div class="1/2 border border-gray-300">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, sapiente!
+            </div>
+            <div class="1/2 border border-gray-300">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita harum, in ipsa iure iusto suscipit.
+            </div>
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "Index",
+
+    layout: MainLayout,
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
